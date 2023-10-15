36657a65787d4f02ece39c307e6b49a00d6ca128
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-15 17:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  // Check if the key exists in the sampleObject
+  return sampleObject.hasOwnProperty(key);
 }
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
